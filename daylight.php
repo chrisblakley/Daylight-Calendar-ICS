724a10577a2bc94ad5879b63713a92126ffc741a
Chris Blakley <greatblakes@gmail.com>
2019-03-05 08:51:19 -0500

    Now grabs the weather for Syracuse.

--- daylight.php
+++ daylight.php
@@ -2,11 +2,15 @@
 /*
 	ICS Validator: http://severinghaus.org/projects/icv/
 	Another Validator: http://icalvalid.cloudapp.net/
+
+	My favorite time of day is 11-14 minutes after sunset (when calculated with zenith 90.83). The beautiful sunset is just finishing up with its deepest colors and streetlights and other man-made lights are on.
+	This is just about (slightly before) halfway between sunset as calculated above and civil sunset (zenith of 96).
+
 */
 
 $debug = 0; //Enable forced debug mode here
 
-if ( $debug == 0 && !array_key_exists('debug', $_GET) ) {
+if ( $debug == 0 && !array_key_exists('debug', $_GET) ){
 	header('Content-type: text/calendar; charset=utf-8');
 	header('Content-Disposition: attachment; filename=gearside_daylight.ics');
 }
@@ -26,6 +30,9 @@ $gmt = ( isset($_GET['gmt']) )? intval($_GET['gmt']) : -5;
 $gmt_math = ($gmt*3600)*-1;
 
 $syracuse = ( $lat == 43.0469 && $lng == -76.1444 ) ? 1: 0;
+if ( $syracuse ){
+	date_default_timezone_set('America/New_York'); //This is only used for the "Last Updated" date
+}
 
 if ( isset($_GET['z']) ) {
 	if ( is_numeric($_GET['z']) ) {
@@ -74,6 +81,72 @@ $longest_sunrise = strtotime($longest_date . ' ' . date_sunrise(strtotime($longe
 $longest_sunset = strtotime($longest_date . ' ' . date_sunset(strtotime($longest_date), SUNFUNCS_RET_STRING, $lat, $lng, $zenith, $gmt));
 $longest_length = $longest_sunset-$longest_sunrise;
 
+//Current weather forecast
+$weather_forecast = array();
+$weather_forecast_summary = '';
+if ( $syracuse ){
+	//Weather Forecast Lookup (Yahoo Weather API)
+	function buildBaseString($baseURI, $method, $params){
+		$r = array();
+		ksort($params);
+		foreach( $params as $key => $value ){
+			$r[] = "$key=" . rawurlencode($value);
+		}
+		return $method . "&" . rawurlencode($baseURI) . '&' . rawurlencode(implode('&', $r));
+	}
+
+	function buildAuthorizationHeader($oauth){
+		$r = 'Authorization: OAuth ';
+		$values = array();
+		foreach( $oauth as $key=>$value ){
+			$values[] = "$key=\"" . rawurlencode($value) . "\"";
+		}
+		$r .= implode(', ', $values);
+		return $r;
+	}
+
+	$url = 'https://weather-ydn-yql.media.yahoo.com/forecastrss';
+	$app_id = 'DeJXrq6s';
+	$consumer_key = 'dj0yJmk9TUdkTURIclp5VFB6JnM9Y29uc3VtZXJzZWNyZXQmc3Y9MCZ4PTU2'; //Client ID
+	$consumer_secret = '624b553e212c750ddfa5212d5442282b2102cd26'; //Client Secret
+	$query = array('location' => 'syracuse,ny', 'format' => 'json');
+
+	$oauth = array(
+		'oauth_consumer_key' => $consumer_key,
+		'oauth_nonce' => uniqid(mt_rand(1, 1000)),
+		'oauth_signature_method' => 'HMAC-SHA1',
+		'oauth_timestamp' => time(),
+		'oauth_version' => '1.0'
+	);
+
+	$base_info = buildBaseString($url, 'GET', array_merge($query, $oauth));
+	$composite_key = rawurlencode($consumer_secret) . '&';
+	$oauth_signature = base64_encode(hash_hmac('sha1', $base_info, $composite_key, true));
+	$oauth['oauth_signature'] = $oauth_signature;
+	$header = array(buildAuthorizationHeader($oauth), 'X-Yahoo-App-Id: ' . $app_id);
+
+	$ch = curl_init();
+	curl_setopt_array($ch, array(
+		CURLOPT_HTTPHEADER => $header,
+		CURLOPT_HEADER => false,
+		CURLOPT_URL => $url . '?' . http_build_query($query),
+		CURLOPT_RETURNTRANSFER => true,
+		CURLOPT_SSL_VERIFYPEER => false
+	));
+	$response = curl_exec($ch);
+	curl_close($ch);
+
+	$weather_forecast_data = json_decode($response);
+	foreach ( $weather_forecast_data->forecasts as $forecast_day ){
+		$weather_forecast['day' . date('Ymd', $forecast_day->date)] = array(
+			'date' => $forecast_day->date, //UTC time
+			'high' => $forecast_day->high . '°F',
+			'low' => $forecast_day->low . '°F',
+			'forecast' => ucwords($forecast_day->text)
+		);
+	}
+}
+
 ?>
 BEGIN:VCALENDAR<?php echo "\r\n"; ?>
 VERSION:2.0<?php echo "\r\n"; ?>
@@ -114,11 +187,23 @@ while (strtotime($date) <= strtotime($year-1 . '-12-31') ) :
 	$dst = ( date('I', strtotime($date . '+1 year +12 hours')) ) ? 1 : 0;
 
 	$solar_noon = ( $dst ) ? $noon+3600 : $noon;
-	$length_percentile = round((($length-$shortest_length)*100)/($longest_length-$shortest_length), 1);
+
+	$length_percentile = 0;
+	if ( $longest_length-$shortest_length > 0 ){ //Prevent division by 0
+		$length_percentile = round((($length-$shortest_length)*100)/($longest_length-$shortest_length), 1);
+	}
+
+	//Add the weather info to the summary if it matches the date correctly
+	if ( !empty($weather_forecast) ){ //If we have weather data
+		$this_weather_day = 'day' . date('Ymd', strtotime($date . '+1 year '));
+		if ( !empty($weather_forecast[$this_weather_day]) ){ //If we have weather data for this specific day
+			$weather_forecast_summary = ' Forecast: ' . $weather_forecast[$this_weather_day]['forecast'] . ', High: ' . $weather_forecast[$this_weather_day]['high'] . ', Low ' . $weather_forecast[$this_weather_day]['low'] . '.';
+		}
+	}
 
 	$last_sync = ( $date == date('Y-m-d', strtotime('Today -1 Year')) && 1==2 ) ? ' [Last Sync]' : '';
 
-	if ( $debug == 1 || array_key_exists('debug', $_GET) ) {
+	if ( $debug == 1 || array_key_exists('debug', $_GET) ){
 		echo "\r\n------------------\r\n";
 		echo ( $date == date('Y-m-d', strtotime('Today -1 Year')) ) ? "(Today!) " : "";
 		echo "Debug Info\r\n";
@@ -139,6 +224,7 @@ while (strtotime($date) <= strtotime($year-1 . '-12-31') ) :
 		echo "Longest day this year: " . $longest_length . "\r\n";
 		echo "This day length: " . $length . "\r\n";
 		echo $length_percentile . " Percentile\r\n";
+		echo ( !empty($weather_forecast_summary) )? $weather_forecast_summary : '(No forecast for this date)';
 
 		echo "\r\n";
 	}
@@ -154,13 +240,13 @@ UID:<?php echo md5(uniqid(mt_rand(), true)) . "@gearside.com" . "\r\n"; ?>
 DESCRIPTION:<?php echo escapeString(
 	"Civil: " . date('g:ia', strtotime(date('F j Y g:ia', $sunrise_civil) . ' +' . $dst . ' hours')) . ' to ' . date('g:ia', strtotime(date('F j Y g:ia', $sunset_civil) . ' +' . $dst . ' hours')) . " (There is enough natural sunlight that artificial light may not be required to carry out human activities.) --- " .
 	"Nautical: " . date('g:ia', strtotime(date('F j Y g:ia', $sunrise_nautical) . ' +' . $dst . ' hours')) . ' to ' . date('g:ia', strtotime(date('F j Y g:ia', $sunset_nautical) . ' +' . $dst . ' hours')) . " (The point at which the horizon stops being visible at sea) --- " .
-	"Astronomical: " . date('g:ia', strtotime(date('F j Y g:ia', $sunrise_astronomical) . ' +' . $dst . ' hours')) . ' to ' . date('g:ia', strtotime(date('F j Y g:ia', $sunset_astronomical) . ' +' . $dst . ' hours')) . " (The point when Sun stops being a source of any illumination) --- " .
-	"Calendar by Gearside.com") . "\r\n"; ?>
+	"Astronomical: " . date('g:ia', strtotime(date('F j Y g:ia', $sunrise_astronomical) . ' +' . $dst . ' hours')) . ' to ' . date('g:ia', strtotime(date('F j Y g:ia', $sunset_astronomical) . ' +' . $dst . ' hours')) . " (The point when Sun stops being a source of any illumination) --- " . ' [Last Updated: ' . date('l, F j, Y g:ia') . ']' .
+	"Calendar by Gearside.com") . "\r\n"; //This is for additional information ?>
 <?php else : ?>
-DESCRIPTION:<?php echo escapeString('Daylight calendar by Gearside') . "\r\n"; ?>
+DESCRIPTION:<?php echo escapeString('Daylight calendar by Gearside.com') . "\r\n"; //This is for additional information ?>
 <?php endif; ?>
 URL;VALUE=URI:<?php echo escapeString('http://gearside.com/calendars/daylight.ics') . "\r\n"; ?>
-SUMMARY:<?php echo escapeString($hours . 'h ' . $minutes . 'm (' . round($percent, 1) . '%) of daylight [' . $length_percentile . ' percentile]. Solar noon at ' . date('g:ia', $solar_noon) . '.' . $last_sync) . "\r\n"; ?>
+SUMMARY:<?php echo escapeString($hours . 'h ' . $minutes . 'm (' . round($percent, 1) . '%) of daylight [' . $length_percentile . ' percentile]. Solar noon at ' . date('g:ia', $solar_noon) . '.' . $weather_forecast_summary . $last_sync) . "\r\n"; //Shows up in the title of the event ?>
 RRULE:FREQ=YEARLY;COUNT=3<?php echo "\r\n"; ?>
 END:VEVENT<?php echo "\r\n"; ?>
 <?php
